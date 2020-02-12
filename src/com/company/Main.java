3321505a41715cf7fad4d0ba72826cbb06e30c0a
mariamworkman <60803434+mariamworkman@users.noreply.github.com>
2020-02-11 18:24:02 -0800

    HW1, ex.1

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+int x;
+    for (x=1; x <=10; x++) {
+        System.out.println (x);
+    }
+
+    }
+}
