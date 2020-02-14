08c6191968b1ec62ffe0b3690984210603a66c2c
mariamworkman <60803434+mariamworkman@users.noreply.github.com>
2020-02-14 01:41:08 -0800

    hm1ex2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int i=1; i<=10; i++){
-            if (i == 7) {
-                continue;
-            } System.out.println(i);
-        }
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(numbers[0]);
 
 
 
