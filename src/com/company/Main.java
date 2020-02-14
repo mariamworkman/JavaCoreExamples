9f9ec3cf9ce111ecb005f273913cf5059675a71f
mariamworkman <60803434+mariamworkman@users.noreply.github.com>
2020-02-14 01:35:25 -0800

    HM1ex1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-int x;
-    for (x=1; x <=10; x++) {
-        System.out.println (x);
-    }
+        for (int i=1; i<=10; i++){
+            if (i == 7) {
+                continue;
+            } System.out.println(i);
+        }
+
+
 
     }
 }
