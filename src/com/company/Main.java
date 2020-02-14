a9fbf5800deab9a795411f7d41ea2c11ac2b10fa
mariamworkman <60803434+mariamworkman@users.noreply.github.com>
2020-02-14 02:02:49 -0800

    hm1ex2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
 
-        try {
-            int[] numbers = {1, 2, 3, 4, 5};
-
-            System.out.println(numbers[7]);
-
-        } catch (Exception e) {
-            System.out.println("Outside of bound message.");
-        }
+    int [] numbers = {1, 2, 3, 4, 5};
+    System.out.println(numbers[1]);
 
     }
 }
